2afcef1e5f55e23b569687fb546574bb341ae859
Markus Bertheau <mbertheau@gmail.com>
2022-12-07 20:36:16 +0100

    Track cumulative reward

--- play.py
+++ play.py
@@ -2,7 +2,7 @@ import time
 
 import gym
 
-env = gym.make("LunarLander-v2")
+env = gym.make("LunarLander-v2", render_mode="rgb_array")
 from gym.utils.play import play, PlayPlot
 import gym.envs.box2d.lunar_lander
 import pygame
@@ -10,9 +10,16 @@ import pygame
 
 gym.envs.box2d.lunar_lander.INITIAL_RANDOM = 10
 
+cum_rew = 0
 
-def callback(obs_t, obs_tp1, action, rew, done, info):
-    return [rew]
+
+def callback(obs_t, obs_tp1, action, rew, terminated, truncated, info):
+    global cum_rew
+    cum_rew += rew
+    if terminated or truncated:
+        print(f"Resetting cumulative reward, reached {cum_rew} this time.")
+        cum_rew = 0
+    return [cum_rew]
 
 
 plotter = PlayPlot(callback, 30 * 5, ["reward"])
